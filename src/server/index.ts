2b69c22d643fac7ab216b7ed616f5d8c1e423066
hpr <habs@sdf.org>
2026-02-07 20:47:14 -0500

    add amqp conn

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,14 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const conn = await amqp.connect("amqp://guest:guest@localhost:5672/");
+  console.log("Connection was successful");
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    await conn.close();
+    process.exit()
+  });
 }
 
 main().catch((err) => {
