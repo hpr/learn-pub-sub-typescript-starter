673741805a50cdeae411f4e86d25aaa9914b6c73
hpr <habs@sdf.org>
2026-02-08 09:47:18 -0500

    add client repl

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,7 @@
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandMove } from "../internal/gamelogic/move.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { amqpConnect } from "../internal/pubsub/connect.js";
 import { declareAndBind } from "../internal/pubsub/pub.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
@@ -7,7 +10,36 @@ async function main() {
   const conn = await amqpConnect();
   const username = await clientWelcome();
   const [ch, queue] = await declareAndBind(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, "transient");
-  
+
+  const gs = new GameState(username);
+  while (true) {
+    const words = await getInput();
+    if (!words.length) continue;
+    switch (words[0]) {
+      case "spawn":
+        commandSpawn(gs, words);
+        break;
+      case "move":
+        commandMove(gs, words);
+        break;
+      case "status":
+        await commandStatus(gs);
+        break;
+      case "help":
+        printClientHelp();
+        break;
+      case "spam":
+        console.log("Spamming not allowed yet!");
+        break;
+      case "quit":
+        printQuit();
+        process.kill(process.pid, "SIGINT");
+        break;
+      default:
+        console.log("I don't understand the command");
+        break;
+    }
+  }
 }
 
 main().catch((err) => {
